743a09021b9a3db8d3c916752514cc4809fda7ed
Raghav Jha <160571389+Raghavjha6@users.noreply.github.com>
2025-01-23 21:33:56 +0530

    Add files via upload

---
+++ Assignment2/conditioncontrol2.py
@@ -0,0 +1,8 @@
+'''Q. Any integer is input through the keyboard. Write a program to find out whether it is an odd 
+number or even number.'''
+
+num = int(input("Enter an integer: "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
